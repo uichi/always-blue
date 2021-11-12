ee10b449051c5ddd3a18146570c73ecfe7b7b768
uichi <curry_and_coffee@ykonishi.tokyo>
2021-11-12 16:07:51 +0900

    Make my site.

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,8 +3,15 @@ import Layout from '../components/layout'
 
 const AboutPage = () => {
   return (
-    <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    <Layout pageTitle="About">
+      <p>ようこそ、いつだって広い海へ</p>
+      <h2>uichi</h2>
+      <p>どうも、本ブログを運営しているフルスタックエンジニアのuichiです。</p>
+      <p>Web制作会社で3年ほど勤め、2020年10月にフリーランスに転身。</p>
+      <p>2021年11月から再び自社開発系の会社で正社員に戻りました。</p>
+      <p>フロントもバックエンドも経験し、デザインもやっていたりとフルフルスタックです。</p>
+      <p>本ブログは主に技術ブログとして活用していこうと考えています。趣味的な話もたまに投稿していきます。</p>
+      <p>趣味は漫画、映画、ドラマ、瞑想、旅行、キャリステクニクスなど幅広いです。最近、東日本を旅してたりもしていました。今期注目しているドラマは日本沈没です。</p>
     </Layout>
   )
 }
