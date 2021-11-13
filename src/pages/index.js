0670b6a6275310e88d6fb3699eb1b690b269e5a5
uichi <37263474+uichi@users.noreply.github.com>
2021-11-14 00:21:51 +0900

    Make home style.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,51 @@
 import * as React from 'react'
-import styled from 'styled-components'
 import { StaticImage } from 'gatsby-plugin-image'
-import Layout from '../components/layout';
+import styled, { createGlobalStyle } from 'styled-components';
+import Seo from '../components/seo'
+import Header from '../components/header';
+import Footer from '../components/footer';
 
-const HomeImage = styled.div`
-  box-shadow: 0 5px 10px 0 rgba(0 ,0, 0, .2);
-  filter: opacity(80%);
-  width: 100%;
-  z-index: -1;
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #e6ffff;
+    margin: 0;
+    padding: 0;
+`;
+const HomeFooter = styled.div`
+  & footer {
+    position: absolute;
+    margin: 0 0 10px 0;
+    bottom: 0;
+    right: 50%;
+    transform: translateX(50%)
+  }
 `
 
 const IndexPage = () => {
   return (
-    <Layout title="ホーム">
-      <HomeImage>
-        <StaticImage
-          alt="いつだって広い海"
-          src="../images/always-blue.jpg"
-        />
-      </HomeImage>
-    </Layout>
+    <>
+      <GlobalStyle />
+      <Header />
+      <Seo title="ホーム" />
+      <StaticImage
+        alt="いつだって広い海"
+        src="../images/always-blue.jpg"
+        style={{
+          boxShadow: "0 5px 10px 0 rgba(0 ,0, 0, .2)",
+          filter: "opacity(80%)",
+          objectPosition: "50% bottom",
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          zIndex: "-1",
+          width: "100%",
+          height: "100vh",
+        }}
+      />
+      <HomeFooter>
+        <Footer />
+      </HomeFooter>
+    </>
   )
 }
 
